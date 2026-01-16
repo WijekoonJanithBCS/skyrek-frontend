78d374be8c22162b00eca3d799ef10b6e7d60b0a
WijekoonJanithBCS <wijekoonjanith72@gmail.com>
2026-01-16 15:38:02 +0530

    commit-08

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,7 +1,8 @@
 import express from 'express';
 
+import { createUser } from '../controllers/userController.js';
 const userRouter = express.Router();
-userRouter.post('/createUser');
+userRouter.post('/',createUser);
 
 export default userRouter;
     
